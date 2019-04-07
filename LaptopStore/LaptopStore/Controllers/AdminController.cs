9ff94ba893e61d4e821fd3f0cf12fdb4c620d581
vinhnguyen080797 <vinhnguyen080797@gmail.com>
2019-04-07 17:20:19 +0700

    add timkiem

--- LaptopStore/LaptopStore/Controllers/AdminController.cs
+++ LaptopStore/LaptopStore/Controllers/AdminController.cs
@@ -21,5 +21,9 @@ namespace LaptopStore.Controllers
         {
             return View();
         }
+        public ActionResult Timkiem()
+        {
+            return View();
+        }
     }
 }
